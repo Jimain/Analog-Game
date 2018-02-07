03c4fc0da03eaf7970959bcaf377a63510e64869
Steven Dufresne <dufresnesteven@gmail.com>
2018-02-07 19:31:00 +0900

    App container setup

--- src/AnalogGame.java
+++ src/AnalogGame.java
@@ -1,9 +1,12 @@
+import org.newdawn.slick.SlickException;
 
 public class AnalogGame {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SlickException {
 		// TODO Auto-generated method stub
-
+		GamePlay gp = new GamePlay("Analog Game");
+				
+		gp.startGame();
 	}
 
 }
--- src/GamePlay.java
+++ src/GamePlay.java
@@ -1,11 +1,23 @@
+import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.StateBasedGame;
 
 public class GamePlay extends StateBasedGame {
 
-	public GamePlay() {
+	public GamePlay(String name) {
+		super(name);
 		// TODO Auto-generated constructor stub
+	}
+
+	public void startGame() throws SlickException {
+		AppGameContainer app = new AppGameContainer(new GamePlay("Analog Game"));
+		
+		app.setDisplayMode(800, 800, false);
+		app.setAlwaysRender(true);
+		
+		app.start();
+		
 	}
 
 	@Override
