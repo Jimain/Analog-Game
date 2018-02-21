6ade9c787dfefcfab40b0a08505bbeaf0d924732
Steven Dufresne <dufresnesteven@gmail.com>
2018-02-21 18:41:29 +0900

    Move the plane logic into its own classq

--- src/Plane.java
+++ src/Plane.java
@@ -1,8 +1,54 @@
+import java.awt.Point;
+
+import org.newdawn.slick.Graphics;
 
 public class Plane {
-
+	private int pHeight = 20;
+	private int pWidth = 50;
+	Point p = new Point();
+	public int mvtExtent = 30;
+	
 	public Plane() {
+		// create new point for plane location
+		p = new Point();
+		p.x = 350;
+		p.y = 350;
+	}
+	
+	public void updateLocation(int key) {
+		switch (key) {
+		case 203:
+			p.x -= mvtExtent;
+			break;
+		case 200:
+			p.y -= mvtExtent;
+			break;
+		case 205:
+			p.x += mvtExtent;
+			break;
+		case 208:
+			p.y += mvtExtent;
+			break;
+		}
+
+		if (p.x >= 750) {
+			p.x = 750;
+		}
+		if (p.x <= 0) {
+			p.x = 0;
+		}
+		if (p.y >= 750) {
+			p.y = 750;
+		}
+		if (p.y <= 0) {
+			p.y = 0;
+		}
 		
+	}
+	
+	public void render(Graphics g) {
+		// render rect in position of point
+		g.fillRect(p.x, p.y, pWidth, pHeight);
 	}
 
 }
--- src/Play.java
+++ src/Play.java
@@ -8,59 +8,26 @@ import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
 public class Play extends BasicGameState implements InputListener {
-	private int pHeight = 20;
-	private int pWidth = 50;
-	Point p = new Point();
-	public int mvtExtent = 30;
+	Plane plane;
 
 	@Override
 	public void init(GameContainer arg0, StateBasedGame arg1) throws SlickException {
 		// TODO Auto-generated method stub
+		plane = new Plane();
 
-		// create new point for plane location
-		p = new Point();
-		p.x = 350;
-		p.y = 350;
-		System.out.println("Init");
 	}
 
 	public void keyPressed(int key, char c) {
-		System.out.println(key);
-		switch (key) {
-		case 203:
-			p.x -= mvtExtent;
-			break;
-		case 200:
-			p.y -= mvtExtent;
-			break;
-		case 205:
-			p.x += mvtExtent;
-			break;
-		case 208:
-			p.y += mvtExtent;
-			break;
-		}
-
-		if (p.x >= 750) {
-			p.x = 750;
-		}
-		if (p.x <= 0) {
-			p.x = 0;
-		}
-		if (p.y >= 750) {
-			p.y = 750;
-		}
-		if (p.y <= 0) {
-			p.y = 0;
-		}
+		plane.updateLocation(key);
+		
 	}
 
 	@Override
 	public void render(GameContainer arg0, StateBasedGame arg1, Graphics g) throws SlickException {
 		// TODO Auto-generated method stub
 
-		// render rect in position of point
-		g.fillRect(p.x, p.y, pWidth, pHeight);
+		plane.render(g);
+		
 	}
 
 	@Override
@@ -74,5 +41,4 @@ public class Play extends BasicGameState implements InputListener {
 		// TODO Auto-generated method stub
 		return 1;
 	}
-
 }
